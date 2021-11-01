07368999c6e47ec9b5e80ed8242aa753c1eeb3d5
ubuntu-user <nezaron@protonmail.com>
2021-11-01 08:09:27 +0100

    Update swap_use.sh

--- swap_use.sh
+++ swap_use.sh
@@ -4,7 +4,7 @@
 
 find /proc -maxdepth 2 -path "/proc/[0-9]*/status" -readable -exec awk -v FS=":" '{process[$1]=$2;sub(/^[ \t]+/,"",process[$1]);} END {if(process["VmSwap"] && process["VmSwap"] != "0 kB") printf "%10s %-30s %20s\n",process["Pid"],process["Name"],process["VmSwap"]}' '{}' \; | awk '{print $(NF-1),$0}' | sort -h | cut -d " " -f2-
 
-# voir https://sleeplessbeastie.eu/2016/12/26/how-to-display-processes-using-swap-space/
+# source : https://sleeplessbeastie.eu/2016/12/26/how-to-display-processes-using-swap-space/
 
 # v0.1 - 2021-0510 - creation
 
