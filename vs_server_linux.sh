65d10fc4d8fe67d4a33493166769673f3a68163a
LemonCurd <nezaron@zaclys.net>
2024-10-26 06:12:59 +0200

    Update vs_server_linux.sh

--- vs_server_linux.sh
+++ vs_server_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 RELEASE_TYPE="stable"
-VERSION="1.18.13"
+VERSION="1.19.8"
 DIRECTORY="vs_server_linux"
 
 wget "https://cdn.vintagestory.at/gamefiles/${RELEASE_TYPE}/vs_server_linux-x64_${VERSION}.tar.gz"
